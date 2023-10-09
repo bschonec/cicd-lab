5258027f873c51e9f4ffa69e8bb37cbe70b10df6
Brian Schonecker <bschonec@gmail.com>
2023-10-09 08:31:54 -0400

    foo

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include cicd_lab
+class cicd_lab {
+  file { '/tmp':
+     ensure => directory,
+  }
+}
